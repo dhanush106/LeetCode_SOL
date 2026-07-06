c0ef044024bfe54d32858748a329e500e235bab2
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2026-07-06 12:02:58 +0530

    Create 112-PathSum

---
+++ 112-PathSum/112-PathSum.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/6/2026, 12:02:58 PM
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) {
+            return false;
+        }
+        
+        if (!root->left && !root->right) {
+            return targetSum == root->val;
+        }
+        
+        bool left_sum = hasPathSum(root->left, targetSum - root->val);
+        bool right_sum = hasPathSum(root->right, targetSum - root->val);
+        
+        return left_sum || right_sum;
+    }
+};
