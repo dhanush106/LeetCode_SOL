474384e8b5ed84bbc070b90a2737a17d26bc1668
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2026-07-06 12:02:00 +0530

    Create 300-LongestIncreasingSubsequence

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
@@ -0,0 +1,18 @@
+# Last updated: 7/6/2026, 12:02:00 PM
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) / 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        return size
+
+        
