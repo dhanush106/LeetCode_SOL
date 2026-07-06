9475ebeea35b8d6c0b551ce40cac79fabe61cddd
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2026-07-06 12:02:31 +0530

    Create 188-BestTimeToBuyAndSellStockIv

---
+++ 188-BestTimeToBuyAndSellStockIv/188-BestTimeToBuyAndSellStockIv.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/6/2026, 12:02:31 PM
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+
+        int n = prices.size();
+
+        vector<vector<int>> prev(2, vector<int>(k+1, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+             vector<vector<int>> curr(2, vector<int>(k+1, 0));
+            for (int cap = 1; cap <= k; cap++) {
+
+                // bUy state
+                curr[1][cap] = max(
+                    -prices[i] + prev[0][cap],
+                    prev[1][cap]
+                );
+
+                curr[0][cap] = max(
+                    prices[i] + prev[1][cap - 1],
+                    prev[0][cap]
+                );
+            }
+            prev = curr;
+        }
+
+        return prev[1][k];
+    }
+};
