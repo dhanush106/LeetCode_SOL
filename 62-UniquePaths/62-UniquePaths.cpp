1901f4e74f644bae93d7f8e2047dcfa78b1968e9
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2026-07-06 12:03:31 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -0,0 +1,47 @@
+// Last updated: 7/6/2026, 12:03:30 PM
+class Solution {
+public:
+    // int solve(int i, int j, vector<vector<int>>& dp){
+    //     if(i==0 && j==0) return 1;
+    //     if(i<0 || j<0) return 0;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int up = solve(i-1,j,dp);
+    //     int left = solve(i,j-1,dp);
+
+    //     return dp[i][j] = up+left;
+    // }
+    int uniquePaths(int m, int n) {
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0) dp[i][j] = 1;
+        //         else{
+        //             int up=0,left=0;
+        //             if(i>0) up = dp[i-1][j];
+        //             if(j>0) left = dp[i][j-1];
+        //             dp[i][j] = up+left;
+        //         }
+        //     }
+        // }
+
+
+        // return dp[m-1][n-1];
+
+        vector<int> dp(n,0);
+        for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) {
+                    temp[j] = 1;
+                    continue;
+                }
+                int up=0,left=0;
+                if(i>0) up = dp[j];
+                if(j>0) left = temp[j-1];
+                temp[j] = up+left;
+            }
+            dp = temp;
+        }
+        return dp[n-1];
+    }
+};
