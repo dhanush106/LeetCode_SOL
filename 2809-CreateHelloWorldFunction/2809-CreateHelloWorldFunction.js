7bddde5882a930875e78820a2ea8308ca618680a
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2026-07-06 12:00:26 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 7/6/2026, 12:00:25 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
