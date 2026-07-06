7bf70786f323a90a6fb95d97d7a617830a8e6560
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2026-07-06 12:02:15 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/6/2026, 12:02:15 PM
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for(int x : nums){
+            if(st.find(x) != st.end()){
+                return true;
+            }
+            st.insert(x);
+        }
+        return false;
+    }
+};
