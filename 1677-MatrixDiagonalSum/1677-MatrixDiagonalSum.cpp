d9c999b660e4083a2e07009cf5c327b7996888d9
dhanush106 <67257018+dhanush106@users.noreply.github.com>
2026-07-06 12:00:47 +0530

    Create 1677-MatrixDiagonalSum

---
+++ 1677-MatrixDiagonalSum/1677-MatrixDiagonalSum.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/6/2026, 12:00:46 PM
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i == j && n-j-1 == i)
+                {
+                    sum = sum + matrix[i][j];
+                }
+                else if(i == j)
+                {
+                    sum = sum + matrix[i][j];
+                }
+                else if(i == n-j-1)
+                {
+                    sum = sum + matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+};
